3eaea9cea8bfa5dff8aaedc310947b9697ad6092
westermank <124292427+westermank@users.noreply.github.com>
2023-02-03 00:49:13 -0600

    Update Ballot.java

--- Ballot.java
+++ Ballot.java
@@ -1,34 +1,17 @@
-import java.util.LinkedList;
+package Voting_Algorithms;
+
 import java.util.ArrayList;
 
 public class Ballot{
-  private String[] candidates = new String[3];
-  private String[] ranking = new String[3];
+  public ArrayList<String> ranking = new ArrayList<String>();
 
   public Ballot(String c1, String c2, String c3){
-    candidates = new String[]{c1,c2,c3};
-    //ranked = new LinkedList<String>();
-  }
-
-  public void list(){
-    System.out.println("The following candidates are running for ellection: "+candidates[0]+", "+candidates[1]+", and "+candidates[2]);
-  }
-  public void rank(String first, String second, String third){
-    ranking = new String[]{first,second,third};
-    //ranked.add(first); will want to decide between linkedlist and ArrayList data structure later
-    //ranked.add(second);
-    //ranked.add(third);
+    ranking.add(c1);
+    ranking.add(c2);
+    ranking.add(c3);
   }
 
   public void ranking(){
-    System.out.println("You ranked the candidates in the following order: first choice "+ranking[0]+", second choice "+ranking[1]+", and third choice "+ranking[2]);
-  }
-
-
-  public static void main(String[] args){
-    Ballot b1 = new Ballot("Bob","Sally","Nelson");
-    b1.list();
-    b1.rank("Sally","Nelson","Bob");//need to validate inputs are contained within Ballot.candidates
-    b1.ranking();
+    System.out.println("You ranked the candidates in the following order: first choice "+ranking.get(0)+", second choice "+ranking.get(1)+", and third choice "+ranking.get(2));
   }
 }
