6062012dabaa85c5ef3ce5b933a23fd7c7adb25d
westermank <124292427+westermank@users.noreply.github.com>
2023-02-07 01:23:00 -0600

    Update Ballot_box.java
    added basic election simulations. need to add multi threading once I figure out why some of my votes are leaking. (around 1-10% in elections with 5 candidates)

--- Ballot_box.java
+++ Ballot_box.java
@@ -3,33 +3,42 @@ package Voting_Algorithms;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map.Entry;
+import java.util.Collections;
 
 public class Ballot_box{
   private HashMap<String, Integer> candidates = new HashMap<String, Integer>();
   private ArrayList<Ballot> ballots = new ArrayList<Ballot>();
 
-  public Ballot_box(String c1, String c2, String c3){
-    candidates.put(c1,0);
-    candidates.put(c2,0);
-    candidates.put(c3,0);
+  public Ballot_box(ArrayList<String> options){
+    for(String option : options)
+      candidates.put(option,0);
   }
 
-  public void vote(String c1, String c2, String c3){
-    if(candidates.containsKey(c1) && candidates.containsKey(c2) && candidates.containsKey(c3)){
-      Ballot vote = new Ballot(c1, c2, c3);
-      ballots.add(vote);
-      candidates.put(c1,candidates.get(c1)+1);
-      System.out.println("Your vote has been successfully submitted!");
+  public void vote(ArrayList<String> choices){
+    ArrayList<String> vote = new ArrayList<String>();
+    for(String choice : choices){
+      if(candidates.containsKey(choice))
+        vote.add(choice);
+      else{
+        System.out.println("One or more of the candidates you entered isn't running check your spelling.");
+        return;
+      }
     }
-    else
-      System.out.println("One or more of the candidates you entered isn't running check your spelling.");
+    Ballot submission = new Ballot(vote);
+    ballots.add(submission);
+    candidates.put(vote.get(0),candidates.get(vote.get(0))+1);
+    System.out.println("Your vote has been successfully submitted! "+submission.ranking.subList(0, submission.ranking.size()));
   }
+
 
   public void list(){
+    int total = 0;
     System.out.println("The following candidates are running for ellection: ");
     for(Entry<String, Integer> entry: candidates.entrySet()){
       System.out.println(entry.getKey()+" with "+entry.getValue()+" votes.");
+      total+=entry.getValue();
     }
+    System.out.println("with a total of "+total+" votes counted.");
   }
 
   public void close(){
@@ -42,28 +51,47 @@ public class Ballot_box{
         System.out.println("Congratulations "+entry.getKey()+" has won the ellection with a majority of "+entry.getValue()+" votes!");
         return;
       }
-      if(biggest_loser == null || entry.getValue() <= candidates.get(biggest_loser))
+      if(biggest_loser == null || entry.getValue() < candidates.get(biggest_loser))
         biggest_loser=entry.getKey();
     }
     list();
     for(Ballot b : ballots){
       if(b.ranking.get(0) == biggest_loser){
+        System.out.println("---------------------");
+        System.out.println(b.ranking.subList(0, b.ranking.size()));
         b.ranking.remove(0);
+        System.out.println(b.ranking.subList(0, b.ranking.size()));
+        System.out.println("---------------------");
+        int i = 0;
+        nextchoice:
         for(String s : b.ranking){
           if(candidates.containsKey(s)){
-            candidates.put(b.ranking.get(0),candidates.get(b.ranking.get(0))+1);
-            break;
-          //I could remove the invalid choices here to minimize potential future loops but that might cause issues while still itterating over the list
+            System.out.println(b.ranking.get(i)+" with "+candidates.get(b.ranking.get(i))+" votes is the next choice.");
+            candidates.put(b.ranking.get(i),candidates.get(b.ranking.get(i))+1);
+            break nextchoice;
+          }
+          else{
+            i++;
           }
         }
       }
     }
-    System.out.println("No majority was found this round so the biggest loser will be eliminated and all of their supporters will get their next valid choice.");
+    System.out.println("No majority was found this round and "+biggest_loser+" is the biggest loser. They will be eliminated and all of their supporters will get their next valid choice.");
     candidates.remove(biggest_loser);
     if(candidates.size() >= 1)
       close();
     else{
       System.out.println("It seems there is only one candidate left.");
+      list();
     }
   }
+
+public void simulate(ArrayList<String> options, Integer Ballots){
+  for (int b = Ballots ; b > 0 ; b--){
+    Collections.shuffle(options);
+    vote(options);
+  }
+  close();
+}
+
 }
