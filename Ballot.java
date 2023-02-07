ec5eb78a8f57b4db6422d094cacf53b58aa0b875
westermank <124292427+westermank@users.noreply.github.com>
2023-02-07 01:20:37 -0600

    Update Ballot.java

--- Ballot.java
+++ Ballot.java
@@ -5,13 +5,12 @@ import java.util.ArrayList;
 public class Ballot{
   public ArrayList<String> ranking = new ArrayList<String>();
 
-  public Ballot(String c1, String c2, String c3){
-    ranking.add(c1);
-    ranking.add(c2);
-    ranking.add(c3);
+  public Ballot(ArrayList<String> choices){
+    for(String choice : choices)
+      ranking.add(choice);
   }
 
   public void ranking(){
-    System.out.println("You ranked the candidates in the following order: first choice "+ranking.get(0)+", second choice "+ranking.get(1)+", and third choice "+ranking.get(2));
+    System.out.println("You ranked the candidates in the following order: "+ranking.subList(0, ranking.size()));
   }
 }
