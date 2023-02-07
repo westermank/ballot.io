3313bd11e980dc51b253f5b82775e9599145c1ab
westermank <124292427+westermank@users.noreply.github.com>
2023-02-07 01:23:38 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,16 +1,17 @@
 import Voting_Algorithms.Ballot_box;
 import Voting_Algorithms.Ballot;
+import java.util.ArrayList;
 
 public class Main{
   public static void main(String[] args){
-    Ballot_box box = new Ballot_box("Bob","Sally","Nelson");
+    ArrayList<String> choices = new ArrayList<String>();
+    choices.add("Bob");
+    choices.add("Sally");
+    choices.add("Nelson");
+    choices.add("Alex");
+    choices.add("Frederick");
+    Ballot_box box = new Ballot_box(choices);
     box.list();
-    box.vote("bob","Sally","Nelson");//test case for input validation
-    box.vote("Sally","Nelson","Bob");
-    box.vote("Nelson","Sally","Bob");//setting up a 2 cycle election where Sally wins after Bob losses in the first round and b3 gets their second vote to give Sally majority win
-    box.vote("Bob","Sally","Nelson");
-    box.vote("Sally","Bob","Nelson");
-    box.vote("Nelson","Bob","Sally");
-    box.close();
+    box.simulate(choices,100);
   }
 }
